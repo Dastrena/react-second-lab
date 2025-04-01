8321e2cfa85ae8f7f7a7dcb4f8ed425a7db7447a
Dastrena <kn23-d.stepanenko@nubip.edu.ua>
2025-04-02 02:20:30 +0300

    Lab 15 complete

---
+++ lab15/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileCards from "./components/Card/Card.jsx";
+
+function App() {
+  return (
+    <div>
+      <ProfileCards />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab15/src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+  background-color: #ede0d4;
+`;
+
